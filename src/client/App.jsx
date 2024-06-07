fe265e78a09351d3af110eb5cc980f468291f0f7
sina-byn <sina.sina.bayan@protonmail.com>
2024-06-07 11:15:31 +0330

    list component wrapped with React.Suspense added to App

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,7 +1,17 @@
 import * as React from 'react';
 
+// * components
+import List from './List';
+
 const App = () => {
-  return <main>this is the app component</main>;
+  return (
+    <main>
+      this is the app component
+      <React.Suspense fallback={<div>Loading the List...</div>}>
+        <List />
+      </React.Suspense>
+    </main>
+  );
 };
 
 export default App;
