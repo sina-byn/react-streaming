10e304bdfa30b602fa3bcee566c5c79f77dec9c3
sina-byn <sina.sina.bayan@protonmail.com>
2024-06-07 10:53:36 +0330

    div with id of app added to be later hydrated

--- src/client/Html.jsx
+++ src/client/Html.jsx
@@ -8,7 +8,9 @@ const Html = ({ children }) => {
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <title>Document</title>
       </head>
-      <body>{children}</body>
+      <body>
+        <div id='app'>{children}</div>
+      </body>
     </html>
   );
 };
