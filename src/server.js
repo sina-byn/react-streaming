12e9e7bd89295903f3954e992f5e0c7e14ab8888
sina-byn <sina.sina.bayan@protonmail.com>
2024-06-07 10:42:58 +0330

    server-side rendering added

--- src/server.js
+++ src/server.js
@@ -1,12 +1,31 @@
 import chalk from 'chalk';
 import express from 'express';
-
-// * initialization
 import { config as env } from 'dotenv';
 
+import * as React from 'react';
+import { renderToPipeableStream } from 'react-dom/server';
+
+// * components
+import Html from './client/Html';
+import App from './client/App';
+
+// * initialization
 env();
 const app = express();
 const port = process.env.PORT ?? 3000;
+
+app.get('/', (_req, res) => {
+  const stream = renderToPipeableStream(
+    <Html>
+      <App />
+    </Html>,
+    {
+      onShellReady() {
+        stream.pipe(res);
+      },
+    }
+  );
+});
 
 app.listen(port, () => {
   console.log(chalk.blueBright(`Server running at http://localhost:${port}`));
